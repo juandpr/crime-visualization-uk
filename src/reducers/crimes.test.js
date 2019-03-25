845d347ea39a7239af2e1584e1f7df7ee80bf1a4
Juan Dalmasso <j_dalmasso@outlook.com>
2019-03-25 18:35:21 +1100

    Added unit tests for crimes reducer

--- src/reducers/crimes.test.js
+++ src/reducers/crimes.test.js
@@ -1,1 +1,23 @@
 import crimes from './crimes';
+import * as actions from '../actions'
+
+it('should return initial state', () => {
+  expect(crimes(undefined, {})).toEqual([]);
+});
+
+it('should handle RECEIVE_CRIMES', () => {
+  const apiResponse = {
+    data: {
+      id: 'crimeId'
+    }
+  }
+  const action = actions.receiveCrimes(apiResponse);
+  expect(crimes(undefined, action)).toEqual({
+    id: 'crimeId'
+  });
+});
+
+it('should handle CLEAR_TABLE', () => {
+  const action = actions.clearTable();
+  expect(crimes(undefined, action)).toEqual([]);
+});
